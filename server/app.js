5a2821d3472d44a5654c2814e9126839ed674e87
Annekay <nwannauk@gmail.com>
2018-05-26 10:02:22 +0100

    create a request for a logged in user

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 //imports from libraries and local files
  import express from 'express';
  
- import routes from './routes/routes';
+ import dbroutes from './routes/dbroutes';
 
  import bodyParser from 'body-parser';
 
  import pool from './database/config';
+ import {createRequest} from './controller/requestController';
 
 
  const app = express();
@@ -16,14 +17,19 @@
     extended: true
   }));
 
- pool.query("INSERT INTO requestLists(title, description, requestType, dateOfRequest) VALUES('fix dixwasher', 'found clog inside of machine', 'urgent', NOW())", (err, res) => {
-     console.log(err, res);
-     pool.end();
- })
+  // pool.query(“CREATE TABLE users(id SERIAL PRIMARY KEY, firstname VARCHAR(40) NOT NULL,
+  // lastName VARCHAR(40) NOT NULL)”, (err, res) => {
+  // console.log(err, res);
+  // pool.end();
+  // });
+// pool.query("CREATE TABLE requests(id SERIAL PRIMARY KEY, user_id INTEGER, title VARCHAR(25) NOT NULL, description VARCHAR(50) NOT NULL, requestType VARCHAR(15), requestdate TIMESTAMP NOT NULL)", (err, res) => {
+//   console.log(err, res);
+//   pool.end();
+// });
 
-  const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
- app.use('/api/v1', routes);
+ app.use('/api/v1', dbroutes);
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
--- server/controller/requestController.js
+++ server/controller/requestController.js
@@ -3,6 +3,7 @@ import requestsModel from './../model/requests';
 import pool from '../database/config';
 import { request } from 'http';
 
+
 //method calls for route handlers
 exports.createRequest = (req, res) =>{
     const {error} = requestsModel(req.body);
@@ -11,12 +12,47 @@ exports.createRequest = (req, res) =>{
         res.status(400).send(error.details[0].message);
         return;
     }
-
-pool.query("INSERT INTO requests(title, description, requestType, dateOfRequest) VALUES('fix dixwasher', 'found clog inside of machine', 'urgent', NOW())", (err, result) => {
-    console.log(err);
-    pool.end();
-
-    res.status(200).send(result);
-})
-
+    pool.query("INSERT INTO requests(user_id, title, description, requestType, requestdate) VALUES(1, 'fixeitoo ', 'found', 'normal', 'NOW()')", (err, result) => {
+        console.log(err, res);
+        pool.end();
+        res.send(result);
+    })
 }    
+
+// exports.getAllRequests = (req,res) => {
+//     res.send(requests);
+// }
+
+
+// exports.putRequest = (req,res) => {
+//     const request = requests.find(r => r.id === parseInt(req.params.id));
+// if(!request) return res.status(404).send('the request with the given id was not found');
+
+// const {error} = validateRequest(req.body);
+// if (error) {
+//     res.status(400).send(error.details[0].message);
+//     return;
+// }
+
+// request.title = req.body.title;
+// request.description = req.body.description;
+
+// res.send(request);
+// }
+
+// exports.getRequestById = (req,res) => {
+    
+//     const request = requests.find(c => c.id === parseInt(req.params.id));
+//     if (!request) return res.status(404).send('the request with the given id does not exist');
+//     res.send(request);
+// }
+
+
+// exports.deleteRequest = (req,res) => {
+//     const request = requests.find(c => c.id === parseInt(req.params.id));
+//     if (!request) return res.status(404).send('the request with the given id does not exist');
+
+//     const index = requests.indexOf(request);
+//     requests.splice(index, 1);
+//     res.send(request);
+// }
