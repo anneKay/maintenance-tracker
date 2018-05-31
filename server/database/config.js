7e064c45fe0707c8c116c50a91ac5ff908fe772e
Annekay <nwannauk@gmail.com>
2018-05-31 18:41:05 +0100

    add host environment variable for database config

--- server/database/config.js
+++ server/database/config.js
@@ -9,9 +9,8 @@ const pool = new pg.Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: '127.0.0.1',
-    //elmer.db.elephantsql.com (elmer-01)
-    port: '5432'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT
 })
 
 export default pool;
