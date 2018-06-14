96e2d45118c22762cf0352085bfddb0549874683
Annekay <nwannauk@gmail.com>
2018-06-14 13:26:54 +0100

    add admin roles

--- ui/js/admin.js
+++ ui/js/admin.js
@@ -26,10 +26,11 @@ window.onload = function() {
        var title = userRequests[i].title;
        var status = userRequests[i].status;
        var requesttype = userRequests[i].requesttype;
+       var id = userRequests[i].id;
        
-       var adminReq = addReq(title, status, requesttype);
+       
+       var adminReq = addReq(title, status, requesttype, id);
        document.getElementById('row').appendChild(adminReq);
-       
    }
      
    })
@@ -46,26 +47,30 @@ window.onload = function() {
    token: localStorage.getItem('authentication'),
    name: localStorage.getItem('name')
    }
-  
  
-function addReq(title, status, requesttype) {
+ 
+function addReq(title, status, requesttype, id) {
  
   var div = document.createElement("DIV");
   div.className = 'col-1-3  remove-gutter-xs admin-requests';
   div.id = 'col-1-3';
+  var link = document.createElement('a');
   var h3 = document.createElement("h3");
   var h5 = document.createElement("h5");
-  var anotherh3 = document.createElement('h5');
   var hr = document.createElement("hr");
   var h4 = document.createElement('h4');
   h3.innerHTML = title;
   h5.innerHTML = status;
   h4.innerHTML = requesttype;
-  
-  div.appendChild(h3);
+  var queryString = "?id=" + id;
+  link.href ="../html/requestdetails.html" + queryString;
+  link.appendChild(h3);
+ link.style.color = "black";
+  div.appendChild(link);
   div.appendChild(h5);
   div.appendChild(hr);
-  div.appendChild(h4)
+  div.appendChild(h4);
+  
   return div;
 }
 
